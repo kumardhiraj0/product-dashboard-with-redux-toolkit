332d67156616bab29128d41cff92bdcdb9cc2a53
Dhiraj kumar <kumardhiraj0248@gmail.com>
2023-08-16 13:39:03 +0530

    added initial setup like layout and raouting part

---
+++ toolkit-reactjs/src/components/NavBarPanel.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+const NavBarPanel = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+          <Nav>
+            <Nav.Link to="/" as = {Link}>Products</Nav.Link>
+          </Nav>
+          <Navbar.Toggle/>
+          <Navbar.Collapse className='justify-content-end'>
+               <Navbar.Text>
+                    <Nav.Link to="/cart" as = {Link}>My Bag 0</Nav.Link>
+               </Navbar.Text>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBarPanel;
